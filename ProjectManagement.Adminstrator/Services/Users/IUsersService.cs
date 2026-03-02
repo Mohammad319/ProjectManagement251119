ccd0e2ecc5a47c899bc36d23f9f52ca79ab11d25
Mohammad319 <mohammad319@gmail.com>
2026-03-02 16:39:07 +0100

    Add tenant action to seed basic company lookup data

--- ProjectManagement.Adminstrator/Services/Users/IUsersService.cs
+++ ProjectManagement.Adminstrator/Services/Users/IUsersService.cs
@@ -19,6 +19,7 @@ namespace ProjectManagement.Adminstrator.Services.Users
         Task<int> CreateAsync(TenantEntity tenant);
         Task<List<GetTenantsDTO>> GetAsync();
         Task<TenantEntity> GetByIdAsync(int id);
+        Task<bool> AddBasicCompanyInfoAsync(int tenantId);
 
     }
 }
