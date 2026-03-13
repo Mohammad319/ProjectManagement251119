fb73479423167c6e8f9b7d7a50efa22341b511a2
Mohammad319 <mohammad319@gmail.com>
2026-03-13 09:17:59 +0100

    Revert "Handle Serilog SQL sink initialization failures gracefully"

--- ProjectManagement.Adminstrator/Program.cs
+++ ProjectManagement.Adminstrator/Program.cs
@@ -61,22 +61,7 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
     options.SupportedUICultures = [.. supportedCultures.Select(c => new CultureInfo(c))];
 });
 
-try
-{
-    Log.Logger = new LoggerConfiguration()
-        .ReadFrom.Configuration(builder.Configuration)
-        .CreateLogger();
-}
-catch (Exception ex)
-{
-    Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Information()
-        .WriteTo.Console()
-        .CreateLogger();
-
-    Log.Warning(ex, "Failed to initialize configured Serilog sinks. Falling back to console logging only.");
-}
-
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
 builder.Host.UseSerilog();
 
 var app = builder.Build();
