b9036b5bbde5ecaab769200a673415246263e606
MohamadAtaya <m4.ataya@gmail.com>
2026-04-15 10:31:53 +0200

    start

--- BlazorMHD.UI/Core/DesignSystem/Shadows.cs
+++
@@ -1,9 +0,0 @@
-namespace BlazorMHD.UI.Core.DesignSystem;
-
-public static class Shadows
-{
-    public static string None => "mhd-shadow-none";
-    public static string Subtle => "mhd-shadow-subtle";
-    public static string Soft => "mhd-shadow-soft";
-    public static string Strong => "mhd-shadow-strong";
-}
--- BlazorMHD.UI/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    darkMode: "class",
-    important: ".mhd-scope",
-    content: ["./**/*.{razor,cs}", "./wwwroot/app.tailwind.css"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
